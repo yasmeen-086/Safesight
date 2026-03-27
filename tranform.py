62888468812a2ed328ec36eb17217e9f8f51be57
Spathneja21 <shubhmpathneja123@gmail.com>
2026-03-28 00:13:16 +0530

    added a basic structure for the bird eye view to map out the ground coordinates.

--- tranform.py
+++ tranform.py
@@ -3,6 +3,26 @@ import numpy as np
 
 image = cv2.imread("image.png")
 
+def get_birdseye_view(frame):
+    src_pts = np.float32([,  # Top-left
+        ,  # Top-right
+        ,  # Bottom-right
+             # Bottom-left
+    ])
+
+    width, height = 500, 600
+    dst_pts = np.float32([,              # Top-left
+        [width, 0],          # Top-right
+        [width, height],     # Bottom-right
+        [0, height]          # Bottom-left
+    ])
+
+    matrix = cv2.getPerspectiveTransform(src_pts, dst_pts)
+
+    warped_image = cv2.warpPerspective(frame, matrix, (width, height), flags=cv2.INTER_LINEAR)
+
+    return warped_image, matrix
+
 src_coords = np.float32([
     [450, 310],  # Top-left
     [820, 310],  # Top-right
